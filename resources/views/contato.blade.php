9cef87b176b365fccb018d1a55f86aba287ab775
Elly <106964692+Bewwwy@users.noreply.github.com>
2023-10-24 08:42:38 -0300

    Add files via upload

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Contato</title>
+</head>
+<body>
+    <div id="container">
+        <nav class="nav">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/produtos">Produtos</a></li>
+                <li><a href="/contato">Contato</a></li>
+            </ul>
+        </nav>
+        <div class="content">
+            <h3>Contato</h3>
+            <img src="/images/banner.jpg" alt="banner laravel" width="40%">
+        </div>
+    </div>
+</body>
+</html>
